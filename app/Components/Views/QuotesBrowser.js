a55faf5bfac50e34e56d3cfe0623897c5024755f
Ionut Mocanu <ionutmocanu.djk@gmail.com>
2016-12-01 03:40:56 +0200

    Remove temp AyncStorage.clear()

--- app/Components/Views/QuotesBrowser.js
+++ app/Components/Views/QuotesBrowser.js
@@ -29,7 +29,7 @@ export default class QuotesBrowser extends React.Component {
 
     componentDidMount() {
         this.getQuotes();
-        AsyncStorage.clear();
+        //AsyncStorage.clear();
     }
 
     getRandom(obj) {
